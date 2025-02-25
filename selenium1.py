c67a7104e37746edcfb3d1e0ebc65b2c97b4dcf5
Dumasuk90 <dimasiks5700@gmail.com>
2025-02-25 18:31:45 +0300

    Update selenium1.py

--- selenium1.py
+++ selenium1.py
@@ -1,1 +1,25 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
+@pytest.fixture(autouse=True)
+def driver():
+
+    driver = webdriver.Chrome()
+    # Переходим на страницу авторизации
+    driver.get('https://petfriends.skillfactory.ru/login')
+
+    yield driver
+    driver.quit()
+
+def test_selenium1(driver):
+    WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.ID, 'email')))
+    driver.find_element(By.ID, 'email').send_keys('dimasiks5700@gmail.com')
+    # Вводим пароль
+    driver.find_element(By.ID, 'pass').send_keys('Dumasuk0209')
+    # Нажимаем на кнопку входа в аккаунт
+    driver.find_element(By.CSS_SELECTOR, 'button[type="submit"]').click()
+    # Проверяем, что мы оказались на главной странице пользователя
+    assert driver.find_element(By.TAG_NAME, 'h1').text == 'PetFriends'
